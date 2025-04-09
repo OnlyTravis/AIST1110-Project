3a105a1320d45b05ca81ab3a706a9c0c90bd91b7
OnlyTravis <TravisTang0626@gmail.com>
2025-04-09 18:02:05 +0800

    Added basic title design

--- src/scenes/title.py
+++ src/scenes/title.py
@@ -14,13 +14,20 @@ class TitleScreen(Scene):
         listener_id = event_handler.addListener(KEYDOWN, self.onKeyDown)
         self.listeners.append(listener_id)
 
-        # temperary
+        # Init texts
         font_1 = font.Font(font.get_default_font(), 40)
-        self.text = font_1.render("Title Screen", True, "black")
+        self.title = font_1.render("Title Screen", True, "black")
+        self.tip =  font_1.render("Press Space to Start", True, "black")
 
     def draw(self):
+        w, h = self.screen.get_size()
+
+        # Draw texts
         self.screen.fill("gray")
-        self.screen.blit(self.text, (self.screen.get_width()/2, self.screen.get_height()/2))
+        title_rect = self.title.get_rect(center=(w/2, h/2))
+        self.screen.blit(self.title, title_rect)
+        tip_rect = self.tip.get_rect(center=(w/2, h/2+100))
+        self.screen.blit(self.tip, tip_rect)
 
     def onKeyDown(self, event: event.Event):
         print(event)
