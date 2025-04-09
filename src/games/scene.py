d48dac9e618bd5e25fb2acffab0287630ee00ee5
OnlyTravis <TravisTang0626@gmail.com>
2025-04-09 17:20:00 +0800

    Added basic stuff

---
+++ src/games/scene.py
@@ -0,0 +1,11 @@
+from collections.abc import Callable
+
+class Scene():
+    def __init__(self, toScene: Callable):
+        self.toScene = toScene
+    
+    def update():
+        pass
+
+    def draw():
+        pass
